3db7b03e07cb5cef8bb08ded325947e69006db1b
Iván M.M <32136734+ivanmmurciaua@users.noreply.github.com>
2022-08-24 17:57:45 +0200

    Greeter deployed in 0xf7BF35b078c2c42A4bC636d5De4d86CfB2Dd33d7

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zkSync-test
+
 Testing with zkSync
+
+Greeter contract : https://zksync2-testnet.zkscan.io/address/0xf7BF35b078c2c42A4bC636d5De4d86CfB2Dd33d7
---
+++ deploy/deploy.ts
@@ -0,0 +1,39 @@
+import { utils, Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Greeter contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet("");
+
+  // Create deployer object and load the artifact of the contract we want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("Greeter");
+
+  // Deposit some funds to L2 in order to be able to perform L2 transactions.
+  const depositAmount = ethers.utils.parseEther("0.001");
+  const depositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: depositAmount,
+  });
+  
+  // Wait until the deposit is processed on zkSync
+  await depositHandle.wait();
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const greeting = "Hi there!";
+  const greeterContract = await deployer.deploy(artifact, [greeting]);
+
+  // Show the contract info.
+  const contractAddress = greeterContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+
+  // Contract Address -> 0xf7BF35b078c2c42A4bC636d5De4d86CfB2Dd33d7
+  // Block Explorer -> https://zksync2-testnet.zkscan.io/address/0x2210cD2Fa5c4D3bdD6916F9cDF9787986B191893
+}
